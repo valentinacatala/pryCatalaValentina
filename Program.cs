27c2ec18111a5b0b2f04512ee75c023296f3d1f7
root <mgiraudo@ies21.edu.ar>
2024-04-03 13:13:44 -0300

    03/04

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace pryCatalaValentina
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmPrincipal());
+            Application.Run(new frmFirma());
         }
     }
 }
--- frmFirma.cs
+++ frmFirma.cs
@@ -12,9 +12,50 @@ namespace pryCatalaValentina
 {
     public partial class frmFirma : Form
     {
+        private Bitmap ArchivoImagen;
         public frmFirma()
         {
             InitializeComponent();
+            ArchivoImagen = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+        }
+
+        private void frmFirma_MouseMove(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void frmFirma_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void frmFirma_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        { 
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                using (Graphics objetoLoco = Graphics.FromImage(ArchivoImagen))
+                {
+                    objetoLoco.FillEllipse(Brushes.Blue, e.X, e.Y, 5, 5);
+                }
+                pictureBox1.Image = ArchivoImagen;
+            }
+        }
+
+        private void btnGrabar_Click(object sender, EventArgs e)
+        {
+            ArchivoImagen.Save("dibujo.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            ArchivoImagen = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = ArchivoImagen;
         }
     }
 }
