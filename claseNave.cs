aede5d52f29e35eb22aa8ae4620c9cfb38e1b7d3
Valentina Catalá <valenncatala@gmail.com>
2024-04-10 00:20:24 -0300

    10/04

---
+++ claseNave.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace pryCatalaValentina
+{
+    internal class claseNave
+    {
+        public int vida;
+        public string nombre;
+        int puntosdaño;
+        public PictureBox imagNave;
+        
+
+        public void CrearJuego()
+        {
+            vida = 100;
+            nombre = "jugador1";
+            puntosdaño = 1;
+
+            imagNave= new PictureBox();
+            imagNave.SizeMode=PictureBoxSizeMode.StretchImage;
+            imagNave.ImageLocation = "https://preview.redd.it/made-the-galaga-ship-in-ksp-v0-v9hgbb8e3hra1.jpg?width=894&format=pjpg&auto=webp&s=3bfb03efb8726c6efa1655e26bdf2bd7c281ac2b";
+
+
+        }
+
+        public void CrearEnemigo()
+        {
+            vida = 25;
+            nombre = "maligno1";
+            puntosdaño = 2;
+
+            imagNave= new PictureBox();
+            imagNave.SizeMode = PictureBoxSizeMode.StretchImage;
+            imagNave.ImageLocation = "https://steamuserimages-a.akamaihd.net/ugc/510378163431176780/C5FF698338D98764BD4A44185C169A7F3B8FE7AD/?imw=637&imh=358&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true";
+
+            
+
+
+
+
+        }
+    }
+}
---
+++ frmJuego.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryCatalaValentina
+{
+    public partial class frmJuego : Form
+    {
+
+        claseNave objNaveJugador;
+        public frmJuego()
+        {
+            InitializeComponent();
+        }
+
+
+        private void frmJuego_Load(object sender, EventArgs e)
+        {
+            objNaveJugador = new claseNave();
+
+            objNaveJugador.CrearJuego();
+            objNaveJugador.imagNave.Location = new Point(550, 550);
+            Controls.Add(objNaveJugador.imagNave);
+
+            objNaveJugador.CrearEnemigo();
+            objNaveJugador.imagNave.Location = new Point(200, 200);
+            Controls.Add(objNaveJugador.imagNave);
+
+
+
+        }
+
+        private void frmJuego_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode==Keys.Right)
+            {
+                objNaveJugador.imagNave.Location = new Point(
+                    objNaveJugador.imagNave.Location.X + 5, objNaveJugador.imagNave.Location.Y);
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                objNaveJugador.imagNave.Location = new Point(
+                    objNaveJugador.imagNave.Location.X - 5, objNaveJugador.imagNave.Location.Y);
+            }
+        }
+    }
+}
+
