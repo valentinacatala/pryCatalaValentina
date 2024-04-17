b20a8409a61d97c11b76903fd9435c7055b16f63
Valentina Catalá <valenncatala@gmail.com>
2024-04-16 22:22:29 -0300

    16/04

---
+++ frmFirmas.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace pryCatalaValentina
+{
+    partial class frmFirmas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new System.Windows.Forms.PictureBox();
+            btnGrabar = new System.Windows.Forms.Button();
+            btnLimpiar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new System.Drawing.Point(34, 27);
+            pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new System.Drawing.Size(539, 481);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            pictureBox1.MouseMove += pictureBox1_MouseMove;
+            // 
+            // btnGrabar
+            // 
+            btnGrabar.Location = new System.Drawing.Point(115, 532);
+            btnGrabar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            btnGrabar.Name = "btnGrabar";
+            btnGrabar.Size = new System.Drawing.Size(112, 31);
+            btnGrabar.TabIndex = 2;
+            btnGrabar.Text = "Grabar";
+            btnGrabar.UseVisualStyleBackColor = true;
+            btnGrabar.Click += btnGrabar_Click;
+            // 
+            // btnLimpiar
+            // 
+            btnLimpiar.Location = new System.Drawing.Point(351, 532);
+            btnLimpiar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            btnLimpiar.Name = "btnLimpiar";
+            btnLimpiar.Size = new System.Drawing.Size(111, 31);
+            btnLimpiar.TabIndex = 3;
+            btnLimpiar.Text = "Limpiar";
+            btnLimpiar.UseVisualStyleBackColor = true;
+            btnLimpiar.Click += btnLimpiar_Click;
+            // 
+            // frmFirmas
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(607, 602);
+            Controls.Add(btnLimpiar);
+            Controls.Add(btnGrabar);
+            Controls.Add(pictureBox1);
+            Name = "frmFirmas";
+            Text = "Dibujar Firma";
+            Load += frmFirmas_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnGrabar;
+        private System.Windows.Forms.Button btnLimpiar;
+    }
+}
--- frmFirmas.cs
+++ frmFirmas.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,29 +11,24 @@ using System.Windows.Forms;
 
 namespace pryCatalaValentina
 {
-    public partial class frmFirma : Form
+    public partial class frmFirmas : Form
     {
         private Bitmap ArchivoImagen;
-        public frmFirma()
+        private string carpetaFirmas;
+        public frmFirmas()
         {
             InitializeComponent();
             ArchivoImagen = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            carpetaFirmas = "firmas";
+            if (!Directory.Exists(carpetaFirmas))
+            {
+                Directory.CreateDirectory(carpetaFirmas);
+            }
         }
 
-        private void frmFirma_MouseMove(object sender, MouseEventArgs e)
+        private void frmFirmas_Load(object sender, EventArgs e)
         {
-        }
 
-        private void frmFirma_Paint(object sender, PaintEventArgs e)
-        {
-        }
-
-        private void frmFirma_Load(object sender, EventArgs e)
-        {
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
-        { 
         }
 
         private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
@@ -41,7 +37,7 @@ namespace pryCatalaValentina
             {
                 using (Graphics objetoLoco = Graphics.FromImage(ArchivoImagen))
                 {
-                    objetoLoco.FillEllipse(Brushes.Blue, e.X, e.Y, 5, 5);
+                    objetoLoco.FillEllipse(Brushes.White, e.X, e.Y, 5, 5);
                 }
                 pictureBox1.Image = ArchivoImagen;
             }
@@ -49,7 +45,11 @@ namespace pryCatalaValentina
 
         private void btnGrabar_Click(object sender, EventArgs e)
         {
-            ArchivoImagen.Save("dibujo.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+            string FechaActual = DateTime.Now.ToString("(yyyy.MM.dd) HH.mm");
+            string nombreArchivo = $"{FechaActual}.png";
+            string Guardar = Path.Combine(carpetaFirmas, nombreArchivo);
+
+            ArchivoImagen.Save(Guardar, System.Drawing.Imaging.ImageFormat.Jpeg);
         }
 
         private void btnLimpiar_Click(object sender, EventArgs e)
