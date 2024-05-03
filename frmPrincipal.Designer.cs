d30f020b4989aff2f3933c788fb5b1d60c1d9ea3
valentincatala <valenncatala>
2024-05-03 12:35:55 -0300

    3/5

--- frmPrincipal.Designer.cs
+++ frmPrincipal.Designer.cs
@@ -30,6 +30,8 @@
         {
             pictureBox1 = new System.Windows.Forms.PictureBox();
             label1 = new System.Windows.Forms.Label();
+            textBox1 = new System.Windows.Forms.TextBox();
+            label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -50,11 +52,28 @@
             label1.AutoSize = true;
             label1.Font = new System.Drawing.Font("Impact", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
             label1.ForeColor = System.Drawing.SystemColors.HighlightText;
-            label1.Location = new System.Drawing.Point(230, 545);
+            label1.Location = new System.Drawing.Point(232, 485);
             label1.Name = "label1";
             label1.Size = new System.Drawing.Size(477, 34);
             label1.TabIndex = 3;
             label1.Text = "PRESIONA ESPACIO PARA EMPEZAR A JUGAR!";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new System.Drawing.Point(449, 572);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(177, 23);
+            textBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+            label2.Location = new System.Drawing.Point(291, 572);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(137, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Ingresar el nombre:";
             // 
             // frmPrincipal
             // 
@@ -64,6 +83,8 @@
             BackgroundImage = Properties.Resources.c54fa1235e6a5abee8c4e8e04aeba201;
             BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             ClientSize = new System.Drawing.Size(884, 961);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
             Controls.Add(label1);
             Controls.Add(pictureBox1);
             DoubleBuffered = true;
@@ -81,5 +102,7 @@
         #endregion
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
